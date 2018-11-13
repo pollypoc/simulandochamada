eb749adece0064161e9add1db4f633ec0ae99b82
pollypoc <lucastecinfo17@gmail.com>
2018-11-12 22:39:54 -0200

    SALVANDO AS COISAS

---
+++ app/src/main/java/com/example/tecinfo/simulandochamada/Main3Activity.java
@@ -0,0 +1,30 @@
+package com.example.tecinfo.simulandochamada;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        TextView txtnumero = findViewById(R.id.txtnumeros);
+        Button ligar = findViewById(R.id.btnligar);
+        Button apagar = findViewById(R.id.btnapagar);
+        Button btn0 = findViewById(R.id.btn0);
+        Button btn1 = findViewById(R.id.btn1);
+        Button btn2 = findViewById(R.id.btn2);
+        Button btn3 = findViewById(R.id.btn3);
+        Button btn4 = findViewById(R.id.btn4);
+        Button btn5 = findViewById(R.id.btn5);
+        Button btn6 = findViewById(R.id.btn6);
+        Button btn7 = findViewById(R.id.btn7);
+        Button btn8 = findViewById(R.id.btn8);
+        Button btn9 = findViewById(R.id.btn9);
+
+    }
+}
